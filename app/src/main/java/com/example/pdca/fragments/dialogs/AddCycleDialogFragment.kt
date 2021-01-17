1f21376bff246a60d928019a863d235f6869c320
yss_miura <jane.was.gentleman@gmail.com>
2021-01-17 20:56:35 +0900

    add DatePickerDialog

--- app/src/main/java/com/example/pdca/fragments/dialogs/AddCycleDialogFragment.kt
+++ app/src/main/java/com/example/pdca/fragments/dialogs/AddCycleDialogFragment.kt
@@ -1,9 +1,12 @@
 package com.example.pdca.fragments.dialogs
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
@@ -17,8 +20,13 @@ import com.example.pdca.data.CycleData
 import com.example.pdca.viewmodels.CycleListViewModel
 import com.example.pdca.viewmodels.ViewModelFactory_CycleList
 import timber.log.Timber
+import java.util.*
 
 class AddCycleDialogFragment(private val cycleData: CycleData, private val isNext: Boolean) : DialogFragment(){
+
+    private var limitedAt: Date? = null
+
+    private lateinit var binding: AddDialogBinding
 
 
     private val viewModel: CycleListViewModel by viewModels {
@@ -30,7 +38,7 @@ class AddCycleDialogFragment(private val cycleData: CycleData, private val isNex
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
-        val binding = DataBindingUtil.inflate<AddDialogBinding>(
+        binding = DataBindingUtil.inflate<AddDialogBinding>(
                 LayoutInflater.from(activity),
                 R.layout.add_dialog,
                 null,
@@ -39,22 +47,24 @@ class AddCycleDialogFragment(private val cycleData: CycleData, private val isNex
 
         val bindingView = binding.root
 
+        binding.inputDateTextView.setOnClickListener {
+            showDatePickerDialog()
+        }
+
         return AlertDialog.Builder(requireContext())
                 .setView(bindingView) // （3） 作成したビューをコンテンツ領域に設定
                 .setPositiveButton(R.string.add_new_cycle) { dialog, which ->
 
                     val new_plan = binding.editPlanFragmentAddDialog.text.toString()
-                    val limit_plan = binding.editLimitFragmentAddDialog.text.toString()
 //
-
-                    if (new_plan.isEmpty() || limit_plan.isEmpty()) {
+                    if (new_plan.isEmpty() || limitedAt == null) {
                         Toast.makeText(activity, R.string.alart_edit_both, Toast.LENGTH_SHORT).show()
                         return@setPositiveButton
                     } else {
                         viewModel.apply {
                             insertCycleData(CycleData().apply {
                                 plan = new_plan
-                                limit = limit_plan
+                                limit = "${DateFormat.format("yyyy/MM/dd", limitedAt)}"
                                 number_of_cycle = cycleData.number_of_cycle+1
                                 if (isNext) {
                                     baseId = cycleData.baseId
@@ -75,5 +85,23 @@ class AddCycleDialogFragment(private val cycleData: CycleData, private val isNex
                     return@setNeutralButton
                 }
                 .create()
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        DatePickerDialog(
+            requireContext(),
+            { _, year, month, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                limitedAt = calendar.time
+                binding.inputDateTextView.text = "${DateFormat.format("yyyy/MM/dd", limitedAt)}"
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH) + 1
+        )
+            .show()
     }
 }
